d024372cb2b62e9a013041ccb5732652074e788e
theonlineaid <nkomorfaruk@gmail.com>
2024-05-09 10:27:38 +0600

    Modify types

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,12 +1,22 @@
 // Create a file (e.g., express.d.ts) in your project's root directory
 
 // Import the express module
+import express from "express";
 import { User } from "@prisma/client";
-import { Request } from 'express';
 
 // Augment the Request interface to include the 'user' property
 declare module 'express' {
     interface Request {
-        user?: any; // Adjust the type of 'user' property as needed
+        user: User; // Adjust the type of 'user' property as needed
     }
 }
+
+
+// import { User } from "@prisma/client";
+// import express  from "express";
+
+// declare module 'express' {
+//     export interface Request {
+//         user: User
+//     }
+// }
