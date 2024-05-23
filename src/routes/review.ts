20d202fddf99bad598bca9aab1cca9499cb60e93
theonlineaid <nkomorfaruk@gmail.com>
2024-05-23 13:00:49 +0600

    Add review image and delete and update functionality but not work properly

--- src/routes/review.ts
+++ src/routes/review.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import authMiddleware from '../middlewares/auth';
 import reviewCtrl from '../controllers/ReviewCtrl';
 import { errorHandler } from '../utils/errorHandler';
-import reviewImageUpload from '../middlewares/upload';
+import createUploadImage from '../middlewares/upload';
+
+const reviewImageUpload = createUploadImage('uploads/reviews/review');
 
 const ReviewRouter = express.Router();
 
 ReviewRouter.post('/:id', [authMiddleware], reviewImageUpload.single('reviewImage'), errorHandler(reviewCtrl.addReview));
 ReviewRouter.delete('/:id', [authMiddleware], errorHandler(reviewCtrl.deleteReview));
-ReviewRouter.patch('/:id', [authMiddleware], errorHandler(reviewCtrl.updateReview));
+ReviewRouter.patch('/:id', [authMiddleware], reviewImageUpload.single('reviewImage'), errorHandler(reviewCtrl.updateReview));
 
 export default ReviewRouter;
