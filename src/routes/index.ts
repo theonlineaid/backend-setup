67d6724652ed69d3fc251a609c301815fd268af6
theonlineaid <nkomorfaruk@gmail.com>
2024-06-20 12:10:09 +0600

    Add all order routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import UserRouter from "./users";
 import UploadRouter from "./upload";
 import CartRouter from "./cart";
 import ReviewRouter from "./review";
+import OrderRouter from "./order";
 
 const RootRouter = Router();
 
@@ -14,6 +15,7 @@ RootRouter.use("/reviews", ReviewRouter)
 RootRouter.use("/users", UserRouter)
 RootRouter.use("/cart", CartRouter)
 RootRouter.use("/upload", UploadRouter)
+RootRouter.use("/orders", OrderRouter)
 
 
 export default RootRouter;
